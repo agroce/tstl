e558d424e18a4ebad42ed5cba882e7339ed253f5
Alex Groce <alexgroce@Alexs-MacBook-Pro.local>
2015-06-19 23:36:18 -0700

    correct messages

--- generators/randombeam.py
+++ generators/randombeam.py
@@ -99,7 +99,7 @@ def handle_failure(test, msg, checkFail):
     
 parsed_args, parser = parse_args()
 config = make_config(parsed_args, parser)
-print('Random testing using config={}'.format(config))
+print('Random variation of beam search using config={}'.format(config))
 
 R = random.Random(config.seed)
 
