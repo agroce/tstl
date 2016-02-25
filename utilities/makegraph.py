d05a0027b34151035a400e8880b6edb3f9818b94
Alex Groce <alexgroce@10-248-126-13.wireless.oregonstate.edu>
2016-02-25 15:27:00 -0800

    graph thing

---
+++ utilities/makegraph.py
@@ -0,0 +1,234 @@
+import sut
+import sys
+from graphviz import Digraph
+import random
+
+def breakByNumber(s):
+    breaks = []
+    curr = ""
+    for c in xrange(0,len(s)):
+        if curr == "":
+            curr += s[c]
+        elif (s[c] in ['0','1','2','3','4','5','6','7','8','9']) == (curr[-1] in ['0','1','2','3','4','5','6','7','8','9']):
+            curr += s[c]
+        else:
+            breaks.append(curr)
+            curr = s[c]
+    if curr != "":
+        breaks.append(curr)
+    return breaks
+
+def breakByRanges(s):
+    breaks = []
+    s2 = str(s)
+    while "<[" in s2:
+        p1 = s2.find("<[")
+        p2 = s2.find(">]")
+        breaks.append(s2[:p1])
+        breaks.append(s2[p1+2:p2])
+        s2 = s2[p2+3:]
+    if s2 != "":
+        breaks.append(s2)
+    return breaks
+
+def merge1(s1,s2):
+    alreadyDiverge = False
+    b1 = breakByNumber(s1)
+    b2 = breakByNumber(s2)
+    if len(b1) != len(b2):
+        return None
+    news = ""
+    for i in xrange(0,len(b1)):
+        if b1[i] != b2[i]:
+            if alreadyDiverge:
+                return None
+            try:
+                v1 = int(b1[i])
+                v2 = int(b2[i])
+                alreadyDiverge = True
+                if (v2 == (v1 + 1)):
+                    news += "<[" + b1[i] + ".." + b2[i] + ">]"
+                else:
+                    return None
+            except ValueError:
+                return None
+        else:
+            news += b1[i]
+    return news
+
+def merge3(s1,s2):
+    alreadyDiverge = False
+    b1 = breakByRanges(s1)
+    b2 = breakByNumber(s2)
+    matching = True
+    news = ""
+    while matching:
+        matching = False
+        if b1[0] == b2[0]:
+            news += b1[0]
+            b1 = b1[1:]
+            b2 = b2[1:]
+            matching = True
+        elif b1[0].find(b2[0]) == 0:
+            news += b2[0]
+            b1[0] = b1[0].split(b2[0],1)[1]
+            if b1[0] == "":
+                b1 = b1[1:]
+            b2 = b2[1:]
+            matching = True
+        elif b2[0].find(b1[0]) == 0:
+            news += b1[0]            
+            b2[0] = b2[0].split(b1[0],1)[1]
+            if b2[0] == "":
+                b2 = b2[1:]
+            b2 = b1[1:]
+            matching = True
+    try:
+        low1 = int(b1[i].split("..")[0])
+        high1 = int(b1[i].split("..")[1])
+        v2 = int(b2[0])
+        if v2 == high2 + 1:
+            news += "<[" + str(low1) + ".." + str(v2) + ">]"
+        b1 = b1[1:]
+        b2 = b2[1:]
+        matching = True
+        while matching:
+            matching = False
+            if b1[0] == b2[0]:
+                news += b1[0]
+                b1 = b1[1:]
+                b2 = b2[1:]
+                matching = True
+            elif b1[0].find(b2[0]) == 0:
+                news += b2[0]
+                b1[0] = b1[0].split(b2[0],1)[1]
+                if b1[0] == "":
+                    b1 = b1[1:]
+                b2 = b2[1:]
+                matching = True
+            elif b2[0].find(b1[0]) == 0:
+                news += b1[0]            
+                b2[0] = b2[0].split(b1[0],1)[1]
+                if b2[0] == "":
+                    b2 = b2[1:]
+                b2 = b1[1:]
+                matching = True
+        if (b1 != []) or (b2 != []):
+            return None
+    except:
+        return None
+    return news
+
+def merge2(s1,s2):
+    alreadyDiverge = False
+    b1 = breakByRanges(s1)
+    b2 = breakByRanges(s2)
+    if len(b1) != len(b2):
+        return None
+    news = ""
+    for i in xrange(0,len(b1)):
+        if b1[i] != b2[i]:
+            if alreadyDiverge:
+                return None
+            try:
+                low1 = int(b1[i].split("..")[0])
+                high1 = int(b1[i].split("..")[1])
+                low2 = int(b2[i].split("..")[0])
+                high2 = int(b2[i].split("..")[1])                                
+                alreadyDiverge = True
+                if (high1 + 1) == low2:
+                    news += "<[" + str(low1) + ".." + str(high2) + ">]"
+                else:
+                    return None
+            except ValueError:
+                return None
+        else:
+            if ".." in b1[i]:
+                news += "<[" + b1[i] + "]>"
+            else:
+                news += b1[i]
+    return news
+
+def collapse(strings):
+    changed = True
+    cstrings = list(strings)
+    while changed:
+        changed = False
+        for s1 in cstrings:
+            for s2 in cstrings:
+                if (s1 == s2):
+                    continue
+                m = merge1(s1,s2)
+                if m != None:
+                    print "MERGED1",s1,"AND",s2,"INTO",m
+                    cstrings.remove(s1)
+                    cstrings.remove(s2)
+                    cstrings.append(m)
+                    changed = True
+                    break
+                m = merge2(s1,s2)
+                if m != None:
+                    print "MERGED2",s1,"AND",s2,"INTO",m
+                    cstrings.remove(s1)
+                    cstrings.remove(s2)
+                    cstrings.append(m)
+                    changed = True
+                    break
+                m = merge3(s1,s2)
+                if m != None:
+                    print "MERGED3",s1,"AND",s2,"INTO",m
+                    cstrings.remove(s1)
+                    cstrings.remove(s2)
+                    cstrings.append(m)
+                    changed = True
+                    break                                
+    return cstrings
+                
+
+outfile = sys.argv[1]
+depth = int(sys.argv[2])
+k = int(sys.argv[3])
+
+dot = Digraph(comment="Depth " + str(depth))
+
+d = 0
+s = 0
+state = "\<init\>"
+dot.node(state, state)
+
+t = sut.sut()
+t.restart()
+
+s = 0
+
+states = []
+
+d = 1
+
+last = state
+
+while d <= depth:
+    nexta = t.enabled()
+    act = random.choice(nexta)
+    aname = t.prettyName(act[0])
+    nexts = map(lambda a: t.prettyName(a[0]), nexta)
+    eqnexts = nexts
+    eqnexts = collapse(eqnexts)
+    eqnexts = filter(lambda x: x != aname, eqnexts)
+    random.shuffle(eqnexts)
+    print eqnexts
+    eqnexts = eqnexts[-(k-1):]
+    mid = len(eqnexts) / 2
+    eqnexts = eqnexts[:mid] + [aname] + eqnexts[mid:]
+    for name in eqnexts:
+        s += 1
+        state = "s" + str(s)
+        dot.node(state,name)
+        if name == aname:
+            newLast = state
+        dot.edge(last,state)
+    last = newLast
+    t.safely(act)
+    d += 1
+
+dot.render(outfile,view=True)
