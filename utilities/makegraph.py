7b6dfc23f7a5e5b9dbaea507d27200cc955c8902
Alex Groce <alexgroce@Alexs-MacBook-Pro.local>
2016-02-26 01:48:01 -0800

    partly working verbose grapher

--- utilities/makegraph.py
+++ utilities/makegraph.py
@@ -23,13 +23,62 @@ def breakByRanges(s):
     s2 = str(s)
     while "<[" in s2:
         p1 = s2.find("<[")
-        p2 = s2.find(">]")
+        p2 = s2.find("]>")
         breaks.append(s2[:p1])
         breaks.append(s2[p1+2:p2])
-        s2 = s2[p2+3:]
+        s2 = s2[p2+2:]
     if s2 != "":
         breaks.append(s2)
     return breaks
+
+def intOrNone(v):
+    try:
+        return int(v)
+    except:
+        return None
+
+def tempMatches(template,list):
+    nonMatch = []
+    abstractions = []
+    tbreaks = breakByNumber(template)
+    indices = {}
+    p = 0
+    for i in tbreaks:
+        if intOrNone(i) != None:
+            indices[p] = [intOrNone(i)]
+            p += 1
+    tindices = filter(lambda x: x != None, map (lambda y: intOrNone(y), tbreaks))
+    allindices = [tindices]
+    for i in list:
+        breaks = breakByNumber(i)
+        if len(breaks) != len(tlist):
+            nonMatch.append(i)
+        else:
+            badMatch = False
+            bindices = []
+            for b in xrange(len(breaks)):
+                if breaks[i] != tbreaks[i]:
+                    v = intOrNone(breaks[i])
+                    tv = intOrNone(tbreaks[i])
+                    if (v == None) or (tv == None):
+                        badMatch = True
+                        break
+                    bindices.append(v)
+        allindices.append(bindices)
+    maxes = list(tindices)
+    mins = list(tindices)
+    for i in allindices:
+        for j in len(i):
+            if j[i] > maxes[i]:
+                maxes[i] = j[i]
+            if j[i] < mins[i]:
+                mins[i] = j[i]                
+    iranges = zip(mins,maxes)
+    rangesets = [iranges]
+    while changed:
+        for irange in rangesets:
+            return None
+    
 
 def merge1(s1,s2):
     alreadyDiverge = False
@@ -47,7 +96,7 @@ def merge1(s1,s2):
                 v2 = int(b2[i])
                 alreadyDiverge = True
                 if (v2 == (v1 + 1)):
-                    news += "<[" + b1[i] + ".." + b2[i] + ">]"
+                    news += "<[" + b1[i] + ".." + b2[i] + "]>"
                 else:
                     return None
             except ValueError:
@@ -56,10 +105,47 @@ def merge1(s1,s2):
             news += b1[i]
     return news
 
+
+
+def merge2(s1,s2):
+    alreadyDiverge = False
+    b1 = breakByRanges(s1)
+    b2 = breakByRanges(s2)
+    if len(b1) != len(b2):
+        return None
+    news = ""
+    for i in xrange(0,len(b1)):
+        if b1[i] != b2[i]:
+            if alreadyDiverge:
+                return None
+            try:
+                low1 = int(b1[i].split("..")[0])
+                high1 = int(b1[i].split("..")[1])
+                low2 = int(b2[i].split("..")[0])
+                high2 = int(b2[i].split("..")[1])                                
+                alreadyDiverge = True
+                if (high1 + 1) == low2:
+                    news += "<[" + str(low1) + ".." + str(high2) + "]>"
+                else:
+                    return None
+            except ValueError:
+                return None
+        else:
+            if ".." in b1[i]:
+                news += "<[" + b1[i] + "]>"
+            else:
+                news += b1[i]
+    return news
+
 def merge3(s1,s2):
+    if ("avl<[0..1]> = avl.AVLTree()" not in s1) or ("avl2 = avl.AVLTree()" not in s2):
+        return None
+    print "3 CHECKING",s1,s2
     alreadyDiverge = False
     b1 = breakByRanges(s1)
     b2 = breakByNumber(s2)
+    print "B1=",b1
+    print "B2=",b2    
     matching = True
     news = ""
     while matching:
@@ -83,16 +169,24 @@ def merge3(s1,s2):
                 b2 = b2[1:]
             b2 = b1[1:]
             matching = True
+    print "AFTER MATCHING:"
+    print "B1=",b1
+    print "B2=",b2
+    print "news=",news
     try:
-        low1 = int(b1[i].split("..")[0])
-        high1 = int(b1[i].split("..")[1])
+        low1 = int(b1[0].split("..")[0])
+        high1 = int(b1[0].split("..")[1])
         v2 = int(b2[0])
-        if v2 == high2 + 1:
-            news += "<[" + str(low1) + ".." + str(v2) + ">]"
+        if v2 == high1 + 1:
+            news += "<[" + str(low1) + ".." + str(v2) + "]>"
         b1 = b1[1:]
         b2 = b2[1:]
+        print "AFTER EXTRACT:"
+        print "B1=",b1
+        print "B2=",b2
+        print "news=",news
         matching = True
-        while matching:
+        while matching and not ((b1 == []) and (b2 == [])):
             matching = False
             if b1[0] == b2[0]:
                 news += b1[0]
@@ -119,43 +213,15 @@ def merge3(s1,s2):
         return None
     return news
 
-def merge2(s1,s2):
-    alreadyDiverge = False
-    b1 = breakByRanges(s1)
-    b2 = breakByRanges(s2)
-    if len(b1) != len(b2):
-        return None
-    news = ""
-    for i in xrange(0,len(b1)):
-        if b1[i] != b2[i]:
-            if alreadyDiverge:
-                return None
-            try:
-                low1 = int(b1[i].split("..")[0])
-                high1 = int(b1[i].split("..")[1])
-                low2 = int(b2[i].split("..")[0])
-                high2 = int(b2[i].split("..")[1])                                
-                alreadyDiverge = True
-                if (high1 + 1) == low2:
-                    news += "<[" + str(low1) + ".." + str(high2) + ">]"
-                else:
-                    return None
-            except ValueError:
-                return None
-        else:
-            if ".." in b1[i]:
-                news += "<[" + b1[i] + "]>"
-            else:
-                news += b1[i]
-    return news
-
 def collapse(strings):
     changed = True
     cstrings = list(strings)
     while changed:
+        print cstrings
         changed = False
         for s1 in cstrings:
             for s2 in cstrings:
+                print "CHECKING",s1,s2
                 if (s1 == s2):
                     continue
                 m = merge1(s1,s2)
@@ -181,13 +247,18 @@ def collapse(strings):
                     cstrings.remove(s2)
                     cstrings.append(m)
                     changed = True
-                    break                                
+                    break
+            if changed:
+                break
     return cstrings
                 
 
 outfile = sys.argv[1]
 depth = int(sys.argv[2])
 k = int(sys.argv[3])
+seed = int(sys.argv[4])
+
+random.seed(seed)
 
 dot = Digraph(comment="Depth " + str(depth))
 
