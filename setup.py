be1f91eb5322c3c06f25f0604c461f5ce722263e
Alex Groce <agroce@gmail.com>
2017-06-25 16:18:08 -0700

    Fix broken pip release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys, os
 
 setup(
     name='tstl',
-    version='1.0.11',
+    version='1.0.12',
     description='Template scripting testing language (TSTL)',
     long_description=open('README.md').read(),
     packages=['src',],
--- src/harnessmaker.py
+++ src/harnessmaker.py
@@ -372,7 +372,7 @@ def main():
     baseIndent = "    "
 
     if "-v" in sys.argv or "--version" in sys.argv:
-        print "TSTL, version 1.0.11"
+        print "TSTL, version 1.0.12"
         print "Documentation at https://github.com/agroce/tstl"
         sys.exit(0)
         
