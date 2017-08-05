c3b4699b448ea4060c8aa516c4bb5022fc2c6bb9
Alex Groce <agroce@gmail.com>
2017-08-05 13:49:15 -0700

    latest tstl for pip

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys, os
 
 setup(
     name='tstl',
-    version='1.0.15',
+    version='1.0.16',
     description='Template scripting testing language (TSTL)',
     long_description=open('README.md').read(),
     packages=['src',],
