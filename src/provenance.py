723228fbe4b64d36e5f6f192a01eb5f4edd8d21e
Alex Groce <agroce@gmail.com>
2017-10-13 17:40:01 -0700

    tool for constructing provenance (just sequence finder for now)

---
+++ src/provenance.py
@@ -0,0 +1,70 @@
+import sys
+import sut
+import glob
+
+sut = sut.sut()
+
+def abstraction(s):
+    if len(s) <= 3:
+        return (sut.actionClass(s),)
+    else:
+        return (sut.actionClass(s),s[3])
+
+seedFiles = sys.argv[1]
+test = sys.argv[2]
+newTest = sys.argv[3]
+
+abstract = "--abstract" in sys.argv
+
+seeds = []
+for f in glob.glob(seedFiles):
+    t = sut.loadTest(f)
+    if abstract:
+        t = map(abstraction,t)
+    seeds.append((t,f))
+
+original = sut.loadTest(test)
+t = list(original)
+if abstract:
+    t = map(abstraction,t)
+
+pos = 0
+
+possible = []
+while pos < len(t):
+    relevant = []
+    for seed in seeds:
+        for i in xrange(0,len(seed[0])):
+            if seed[0][i][0] == t[pos][0]:
+                relevant.append((seed,i))
+    print pos,t[pos][0],map(lambda x:(x[0][1],x[1]),relevant)
+    pos += 1
+    possible.append(relevant)
+
+print
+print
+    
+pos = 1
+previous = possible[0]
+while pos < len(possible):
+    endPos = pos
+    ok = True
+    while ok:
+        ok = False
+        preImageReduce = filter(lambda x:(x[0],x[1]-1) in previous, possible[endPos])
+        if len(preImageReduce) > 0:
+            endPos += 1
+            ok = True
+            previous = preImageReduce
+        else:
+            print pos-1,"-",endPos-1,map(lambda x:x[0][1],previous)
+            for i in xrange(pos-1,endPos):
+                print "    ",i,t[i][0]
+    previous = possible[endPos]
+    pos = endPos + 1
+        
+    
+    
+        
+    
+    
