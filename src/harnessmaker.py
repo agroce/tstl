3198badc411890fc8fc53ae354f282e261333d30
Alex Groce <agroce@gmail.com>
2018-03-15 16:28:48 -0700

    fix version info

--- src/harnessmaker.py
+++ src/harnessmaker.py
@@ -382,7 +382,7 @@ def main():
     baseIndent = "    "
 
     if "-v" in sys.argv or "--version" in sys.argv:
-        print("TSTL, version 1.1.8")
+        print("TSTL, version 1.1.12")
         print("Documentation at https://github.com/agroce/tstl")
         sys.exit(0)
         
