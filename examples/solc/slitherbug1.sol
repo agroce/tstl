46e0fcbb141437280a712df38782122adc2fd8f6
Alex Groce <agroce@gmail.com>
2018-03-20 18:24:34 -0700

    causes exception in slither

---
+++ examples/solc/slitherbug1.sol
@@ -0,0 +1,425 @@
+contract c {
+function f() returns (int) {
+do
+if (true == true == true)
+if (true == true)
+do
+if ((true) == true)
+if (true)
+do
+continue;
+while ((true));
+
+else
+if ((true) == true)
+do
+do
+continue;
+while ((true));
+
+while (!((true)));
+
+else
+do
+continue;
+while ((true));
+
+
+
+
+while ((true) == true == true);
+
+else
+if ((true) == true == true)
+if ((true) == true)
+if (true)
+do
+continue;
+while ((true));
+
+else
+if ((true) == true)
+do
+do
+continue;
+while ((true));
+
+while (!((true)));
+
+else
+do
+continue;
+while ((true));
+
+
+
+
+else
+if (true)
+do
+continue;
+while ((true));
+
+else
+if ((true) == true)
+do
+do
+continue;
+while ((true));
+
+while (!((true)));
+
+else
+do
+continue;
+while ((true));
+
+
+
+
+
+else
+do
+if (true)
+do
+continue;
+while ((true));
+
+else
+if ((true) == true)
+do
+do
+continue;
+while ((true));
+
+while (!((true)));
+
+else
+do
+continue;
+while ((true));
+
+
+
+while ((true) == true);
+
+
+while (true == true == true == true == true);
+
+
+}
+
+
+function i(int input) returns (int) {
+if (true == true)
+if (true)
+if (true)
+do
+continue;
+while ((true));
+
+else
+if ((true) == true)
+do
+do
+continue;
+while ((true));
+
+while (!((true)));
+
+else
+do
+continue;
+while ((true));
+
+
+
+else
+if ((true) == true == true)
+if ((true) == true)
+if (true)
+do
+continue;
+while ((true));
+
+else
+if ((true) == true)
+do
+do
+continue;
+while ((true));
+
+while (!((true)));
+
+else
+do
+continue;
+while ((true));
+
+
+
+
+else
+if (true)
+do
+continue;
+while ((true));
+
+else
+if ((true) == true)
+do
+do
+continue;
+while ((true));
+
+while (!((true)));
+
+else
+do
+continue;
+while ((true));
+
+
+
+
+
+else
+do
+if (true)
+do
+continue;
+while ((true));
+
+else
+if ((true) == true)
+do
+do
+continue;
+while ((true));
+
+while (!((true)));
+
+else
+do
+continue;
+while ((true));
+
+
+
+while ((true) == true);
+
+
+
+}
+
+
+function h(int input) returns (int) {
+if (true == true == true == true)
+if (true == true)
+if (true)
+if (true)
+do
+continue;
+while ((true));
+
+else
+if ((true) == true)
+do
+do
+continue;
+while ((true));
+
+while (!((true)));
+
+else
+do
+continue;
+while ((true));
+
+
+
+else
+if ((true) == true == true)
+if ((true) == true)
+if (true)
+do
+continue;
+while ((true));
+
+else
+if ((true) == true)
+do
+do
+continue;
+while ((true));
+
+while (!((true)));
+
+else
+do
+continue;
+while ((true));
+
+
+
+
+else
+if (true)
+do
+continue;
+while ((true));
+
+else
+if ((true) == true)
+do
+do
+continue;
+while ((true));
+
+while (!((true)));
+
+else
+do
+continue;
+while ((true));
+
+
+
+
+
+else
+do
+if (true)
+do
+continue;
+while ((true));
+
+else
+if ((true) == true)
+do
+do
+continue;
+while ((true));
+
+while (!((true)));
+
+else
+do
+continue;
+while ((true));
+
+
+
+while ((true) == true);
+
+
+
+
+}
+
+
+function g(int input) returns (int) {
+if (true == true == true == true)
+if (true == true)
+if (true)
+if (true)
+do
+continue;
+while ((true));
+
+else
+if ((true) == true)
+do
+do
+continue;
+while ((true));
+
+while (!((true)));
+
+else
+do
+continue;
+while ((true));
+
+
+
+else
+if ((true) == true == true)
+if ((true) == true)
+if (true)
+do
+continue;
+while ((true));
+
+else
+if ((true) == true)
+do
+do
+continue;
+while ((true));
+
+while (!((true)));
+
+else
+do
+continue;
+while ((true));
+
+
+
+
+else
+if (true)
+do
+continue;
+while ((true));
+
+else
+if ((true) == true)
+do
+do
+continue;
+while ((true));
+
+while (!((true)));
+
+else
+do
+continue;
+while ((true));
+
+
+
+
+
+else
+do
+if (true)
+do
+continue;
+while ((true));
+
+else
+if ((true) == true)
+do
+do
+continue;
+while ((true));
+
+while (!((true)));
+
+else
+do
+continue;
+while ((true));
+
+
+
+while ((true) == true);
+
+
+
+
+}
+
+}
