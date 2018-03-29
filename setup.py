326a6ef7de207e99f096da82caaf761bff435d77
Alex Groce <agroce@gmail.com>
2018-03-29 13:43:30 -0700

    new pip with better readme

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys, os
 
 setup(
     name='tstl',
-    version='1.2.3',
+    version='1.2.4',
     description='Template scripting testing language (TSTL)',
     long_description=open('README.md').read(),
     packages=['src',],
