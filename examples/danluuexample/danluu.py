4d1ebd272f1ec91c18e4b5b5485ef6ef80e58aaf
Alex Groce <agroce@gmail.com>
2018-04-18 23:12:20 -0700

    typo

--- examples/danluuexample/danluu.py
+++ examples/danluuexample/danluu.py
@@ -2,7 +2,7 @@
 
 # TSTL, even with coverage-driven mode, has trouble with this.
 # tstl_afl_fuzz, however, given the start (once TSTL finds -1 in the first
-# position, finishes it in less than five minutes; it does have some difficulty
+# position) finishes it in less than five minutes; it does have some difficulty
 # getting to the first -1 with just an input producing a four item list, though.
 
 def dlfilter(x):
