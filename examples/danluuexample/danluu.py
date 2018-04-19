043aef134ec7370ca20147065a540eca3768f450
Alex Groce <agroce@gmail.com>
2018-04-18 23:16:23 -0700

    More details on TSTL

--- examples/danluuexample/danluu.py
+++ examples/danluuexample/danluu.py
@@ -2,8 +2,11 @@
 
 # TSTL, even with coverage-driven mode, has trouble with this.
 # tstl_afl_fuzz, however, given the start (once TSTL finds -1 in the first
-# position) finishes it in less than five minutes; it does have some difficulty
+# position) finishes it in less than five minutes; it does have _some_ difficulty
 # getting to the first -1 with just an input producing a four item list, though.
+# Still, it can take the four item list easily produced in two minutes of TSTL
+# exploration and reach the target in less than ten minutes.  TSTL doesn't manage
+# that in two hours, using the simple coverage-driven GA.
 
 def dlfilter(x):
     for i in range(0,16):
