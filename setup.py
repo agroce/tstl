ee9589ab8e2ab2d25ffbb63f1195a8ff5dcda3b5
Alex Groce <agroce@gmail.com>
2018-04-25 16:13:40 -0700

    new pip

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys, os
 
 setup(
     name='tstl',
-    version='1.2.14',
+    version='1.2.15',
     description='Template scripting testing language (TSTL)',
     long_description=open('README.md').read(),
     packages=['src',],
