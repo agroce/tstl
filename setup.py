a799e4f48b5a8dd6e8776cecd4acb3fa58bcd0bd
Alex Groce <agroce@gmail.com>
2018-04-26 11:46:37 -0700

    new pip for afl memory limit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys, os
 
 setup(
     name='tstl',
-    version='1.2.15',
+    version='1.2.16',
     description='Template scripting testing language (TSTL)',
     long_description=open('README.md').read(),
     packages=['src',],
--- src/harnessmaker.py
+++ src/harnessmaker.py
@@ -388,7 +388,7 @@ def main():
     baseIndent = "    "
 
     if "-v" in sys.argv or "--version" in sys.argv:
-        print("TSTL, version 1.2.15")
+        print("TSTL, version 1.2.16")
         print("Documentation at https://github.com/agroce/tstl")
         sys.exit(0)
         
