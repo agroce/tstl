125d93ee2d73b0d643c4a84e7f1043494874aa26
Alex Groce <agroce@gmail.com>
2018-04-27 10:39:14 -0700

    new pip release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys, os
 
 setup(
     name='tstl',
-    version='1.2.16',
+    version='1.2.17',
     description='Template scripting testing language (TSTL)',
     long_description=open('README.md').read(),
     packages=['src',],
--- src/harnessmaker.py
+++ src/harnessmaker.py
@@ -388,7 +388,7 @@ def main():
     baseIndent = "    "
 
     if "-v" in sys.argv or "--version" in sys.argv:
-        print("TSTL, version 1.2.16")
+        print("TSTL, version 1.2.17")
         print("Documentation at https://github.com/agroce/tstl")
         sys.exit(0)
 
