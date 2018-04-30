agroce/tstl

commit cc4a93d22ca14a57a97ac0134b164f6cf3c2b869

tstl_afl_fuzz: don't use shell for subprocesses
This is necessary to make P.kill() terminate the process that actually
does the job, rather than the parent shell.

src/tstl_afl_fuzz.py
@@ -77,20 +77,20 @@ def main():
 
     if corpusTimeout:
         start = time.time()
-        corpusCmd = "tstl_aflcorpus " + config.input + " " + str(config.depth) + " " + str(config.corpusBudget)
+        corpusCmd = ["tstl_aflcorpus", config.input, str(config.depth), str(config.corpusBudget)]
         if config.burst:
-            corpusCmd += " --burst"
+            corpusCmd += ["--burst"]
         if config.swarm:
-            corpusCmd += " --swarm"
+            corpusCmd += ["--swarm"]
         if config.noCheck:
-            corpusCmd += " --noCheck"
+            corpusCmd += ["--noCheck"]
         if config.noCover:
-            corpusCmd += " --noCover"
+            corpusCmd += ["--noCover"]
         if config.noReduce:
-            corpusCmd += " --noReduce"
+            corpusCmd += ["--noReduce"]
         if config.skipFails:
-            corpusCmd += " --skipFails"
-        P = subprocess.Popen([corpusCmd], shell=True)
+            corpusCmd += ["--skipFails"]
+        P = subprocess.Popen(corpusCmd)
         while (time.time() - start) < corpusTimeout:
             time.sleep(1)
         if P.poll() is None:
@@ -100,23 +100,23 @@ def main():
             except OSError:
                 pass
 
-    aflCmd = "py-afl-fuzz -t " + config.aflTimeout + " -m " + str(config.aflMemory) + " -i " + config.input + " -o " + config.output
+    aflCmd = ["py-afl-fuzz", "-t", config.aflTimeout, "-m", str(config.aflMemory), "-i", config.input, "-o", config.output]
     if not config.thorough:
-        aflCmd += " -d"
-    aflCmd += " -- tstl_afl"
+        aflCmd += ["-d"]
+    aflCmd += ["--", "tstl_afl"]
     if config.swarm:
-        aflCmd += " --swarm"
+        aflCmd += ["--swarm"]
     if config.noCheck:
-        aflCmd += " --noCheck"
+        aflCmd += ["--noCheck"]
     if config.persist:
-        aflCmd += " --persist"
+        aflCmd += ["--persist"]
     if not config.instrumentAll:
         os.putenv("PYTHON_AFL_TSTL","TRUE")
     else:
         os.unsetenv("PYTHON_AFL_TEST")
     print ("RUNNING AFL WITH COMMAND LINE:",aflCmd)
     start = time.time()
-    P = subprocess.Popen([aflCmd], shell=True)
+    P = subprocess.Popen(aflCmd)
     while (time.time() - start) < (config.timeout - config.corpusBudget):
         time.sleep(1)
     P.kill()

commit e00b37e996b082b2e3b80e2663c27bccdc11e97c

tstl_afl_fuzz: don't log py-afl-fuzz cmdline in Python syntax
Log py-afl-fuzz command-line in a human-readable and copy-and-paste-able
format, rather than as a Python list repr.

Fixes regression introduced in cc4a93d22ca14a57a97ac0134b164f6cf3c2b869.

Bad:

    RUNNING AFL WITH COMMAND LINE: ['py-afl-fuzz', '-t', '5000', '-m', '4096', '-i', 'in', '-o', 'out', '-d', '--', 'tstl_afl', '--persist']

Good:

    RUNNING AFL WITH COMMAND LINE: py-afl-fuzz -t 5000 -m 4096 -i in -o out -d -- tstl_afl --persist

src/tstl_afl_fuzz.py
@@ -111,11 +111,12 @@ def main():
         aflCmd += ["--noCheck"]
     if config.persist:
         aflCmd += ["--persist"]
+    aflCmdStr = " ".join(aflCmd)
     if not config.instrumentAll:
         os.putenv("PYTHON_AFL_TSTL","TRUE")
     else:
         os.unsetenv("PYTHON_AFL_TSTL")
-    print ("RUNNING AFL WITH COMMAND LINE:",aflCmd)
+    print ("RUNNING AFL WITH COMMAND LINE:",aflCmdStr)
     start = time.time()
     P = subprocess.Popen(aflCmd)
     while (time.time() - start) < (config.timeout - config.corpusBudget):