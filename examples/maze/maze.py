7a9302241947b60e568ff66447e5d783b28ccd3a
Alex Groce <agroce@gmail.com>
2018-05-02 12:01:54 -0700

    fix up indentation for Py3

--- examples/maze/maze.py
+++ examples/maze/maze.py
@@ -12,46 +12,46 @@ mtext = '''
 
 mlist = []
 for l in mtext.split("\n")[1:]:
-   mlist.append(l)
+    mlist.append(l)
 
-start = (1,0)
+start = (1, 0)
+
 
 def drawMaze(pos):
     print ()
-    print ("X"*20)
+    print ("X" * 20)
     py = 0
     for l in mlist:
-    	px = 0
-    	for c in l:
-	    if (px,py) == pos:
-	       print ("?",end=" ")
-	    else:
-	       print (c,end=" ")
-	    px += 1
-	print
-	py += 1
-    print ("X"*20)
-	
-        
+        px = 0
+        for c in l:
+            if (px, py) == pos:
+                print ("?", end=" ")
+            else:
+                print (c, end=" ")
+            px += 1
+        print
+        py += 1
+    print ("X" * 20)
 
-def tryMove(m,pos):
-    (x,y) = pos
+
+def tryMove(m, pos):
+    (x, y) = pos
     if m == "U":
-       newPos = (x,y-1)
+        newPos = (x, y - 1)
     elif m == "D":
-       newPos = (x,y+1)
+        newPos = (x, y + 1)
     elif m == "L":
-       newPos = (x-1,y)
+        newPos = (x - 1, y)
     elif m == "R":
-       newPos = (x+1,y)
+        newPos = (x + 1, y)
     else:
-       return pos
-    (x,y) = newPos
+        return pos
+    (x, y) = newPos
     if (x < 0) or (y < 0) or (y >= len(mlist)) or (x >= len(mlist[0])):
-       return pos
+        return pos
     if mlist[y][x] == "*":
-       assert False
+        assert False
     if mlist[y][x] != " ":
-       return pos
+        return pos
     drawMaze(newPos)
     return newPos
