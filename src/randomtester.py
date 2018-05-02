401c3a6f55b0960fe9f6911eff6c85e653918892
Alex Groce <agroce@gmail.com>
2018-05-02 15:00:55 -0700

    fail on loss of failure

--- src/randomtester.py
+++ src/randomtester.py
@@ -684,6 +684,7 @@ def handle_failure(
             traceback.print_tb(f[2], file=sys.stdout)
         else:
             print("NO FAILURE!")
+            assert sut.failsAny(test)
     sys.stdout.flush()
     if (not newCov) and config.multiple:
         if (test in [x[0] for x in failures]) or (
