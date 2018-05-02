3e538af6b40143c1ec727b75c235bb8adb972db9
Alex Groce <agroce@gmail.com>
2018-05-01 18:17:50 -0700

    try running tests in examples

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -19,7 +19,7 @@ class TestExamples(TestCase):
             os.chdir("../..")
 
     def test_examples(self):
-        noTests = True
+        noTests = False
 
         skipTravis = [
             "redis",
