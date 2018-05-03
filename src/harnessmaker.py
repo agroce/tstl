dd37414a44f44eafaa14408925887fb9dac8ff13
Alex Groce <agroce@gmail.com>
2018-05-03 15:32:27 -0700

    new pip release with multiple bugfixes and features

--- src/harnessmaker.py
+++ src/harnessmaker.py
@@ -444,7 +444,7 @@ def main():
     baseIndent = "    "
 
     if "-v" in sys.argv or "--version" in sys.argv:
-        print("TSTL, version 1.2.18")
+        print("TSTL, version 1.2.19")
         print("Documentation at https://github.com/agroce/tstl")
         sys.exit(0)
 
