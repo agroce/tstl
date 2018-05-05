e396cc56bc3e9ad80bc794e51f5925d68ea4885d
Alex Groce <agroce@users.noreply.github.com>
2018-05-05 15:06:19 -0700

    remove slow eval from smallcheck runs

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -61,6 +61,7 @@ class TestExamples(TestCase):
             "bidict",
             "biopython",
             "danluuexample",
+            "eval",
             "tensorflow"]
 
         shouldFail = [
