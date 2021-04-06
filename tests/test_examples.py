73d585e4ed76fa5dae03e4f7b2120167db9614a4
agroce <agroce@gmail.com>
2021-04-06 11:38:13 -0700

    also ignore newsortedcontainers

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -35,6 +35,7 @@ class TestExamples(TestCase):
             "simplejson",
             "solc",
             "vyper",
+            "newsortedcontainers",
             "sortedcontainers",
             "tensorflow",
             "pystan",
